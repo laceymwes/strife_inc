a2fa5876b66897a51ceda02c807b27d4aa292cf5
Michael Lacey <laceymwes@gmail.com>
2018-02-19 11:40:02 -0700

    Update README with new changes

--- strifeserver.py
+++ strifeserver.py
@@ -10,7 +10,6 @@ import os
 import requests
 from google.oauth2 import id_token
 from google.auth.transport import requests as grequests
-import googleapiclient.discovery
 import json
 import uuid
 
